e0288705fc72c8e78ae44ea19ff92a4f605e758b
Jude Kuti <judekuti@gmail.com>
2018-03-19 06:05:09 -0500

    Include router-outlet and remove app-product selector

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductsComponent } from './products/products.component';
 
-const routes = Routes = [
+const routes: Routes = [
 {
     path: 'products',
     component: ProductsComponent
