8d97aa016177f68cec5b4449689a464f508997e0
Jude Kuti <judekuti@gmail.com>
2018-03-19 01:51:49 -0500

    Populate Page with array of products using *ngFor

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -7,12 +7,24 @@ import { Product } from '../product'
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
-    product: Product = {
+    products: Product[] = [{
         id: 1,
         name:"Tripod Stand",
         price: 200,
         category: "Support Aparatus"
-    }
+    },
+    {
+        id: 2,
+        name: "Testtube Rack",
+        price: 40,
+        category: "Support Aparatus"
+    },
+    {
+        id: 3,
+        name: "Conical Flask",
+        price: 50,
+        category: "Measuring Aparatus"
+    }]
 
   constructor() { }
 
