8f5bdb4d8f5b6599c14cdb939efcf149d9ea1c70
Jude Kuti <judekuti@gmail.com>
2018-03-19 00:46:55 -0500

    Initial Commit

---
+++ src/app/products/products.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../product'
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css']
+})
+export class ProductsComponent implements OnInit {
+    product: Product
+
+  constructor() { }
+
+  ngOnInit() {
+      console.log("Hey what's up")
+  }
+
+}
